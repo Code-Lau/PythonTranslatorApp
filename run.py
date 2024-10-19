83729a977f323ce676a33fa1df22a965031c0d46
Lauren Woods <laurenleighwoods@gmail.com>
2024-10-19 16:32:17 +0000

    Add speech recognition code

--- run.py
+++ run.py
@@ -1,37 +1,95 @@
-# How to create translator using Python (https://www.youtube.com/watch?v=3ydfbFFrPWE&list=PLl316cKxhMxtOWHa88kDqm42uWz1aqGfD&index=5)
-from tkinter import *
+#Real time translator using Python (https://www.geeksforgeeks.org/real-time-translation-app-using-python/)
 from tkinter import ttk,messagebox
-import googletrans
+from googletrans import LANGUAGES, Translator
 import textblob
+#How to Build a Language Translator with Text and Audio Using Python and Google APIs (https://medium.com/@nikitasilaparasetty/how-to-build-a-language-translator-with-text-and-audio-using-python-and-google-apis-e2697a97b969)
+from gtts import gTTS
+import os
+import speech_recognition as spr
 
 root = Tk()
 root.title("Google Translator")
 root.geometry("1080x400")
+root.resizable(0, 0)
 
-def label_change():
-    c=combo1.get()
-    c1=combo2.get()
-    label1.configure(text=c)
-    label2.configure(text=c1)
-    root.after(1000,label_change)
+#How to create translator using Python (https://www.youtube.com/watch?v=3ydfbFFrPWE&list=PLl316cKxhMxtOWHa88kDqm42uWz1aqGfD&index=5)
+#Translations
+class Translations:
+    def label_change():
+        c=combo1.get()
+        c1=combo2.get()
+        label1.configure(text=c)
+        label2.configure(text=c1)
+        root.after(1000,label_change)
 
-def translate_now():
-    global language
-    try:
-        text_=text1.get(1.0,END)
-        c2=combo1.get()
-        c3=combo2.get()
-        if(text_):
-            words=textblob.TextBlob(text_)
-            lan=words.detect_language()
-            for i,j in language.items():
-                if(j==c3):
-                    lan_=i
-            words=words.translate(from_lang=lan,to=str(lan_))
-            text2.delete(1.0,END)
-            text2.insert(END,words)
-    except Exception as e:
-        messagebox.showerror("googletrans","Error, please try again")
+    def translate_now():
+        global language
+        try:
+            text_=text1.get(1.0,END)
+            c2=combo1.get()
+            c3=combo2.get()
+            if(text_):
+                words=textblob.TextBlob(text_)
+                lan=words.detect_language()
+                for i,j in language.items():
+                    if(j==c3):
+                        lan_=i
+                words=words.translate(from_lang=lan,to=str(lan_))
+                text2.delete(1.0,END)
+                text2.insert(END,words)
+        except Exception as e:
+            messagebox.showerror("googletrans","Error, please try again")
+
+#Language Translator Using Google API in Python (https://www.geeksforgeeks.org/language-translator-using-google-api-in-python/)
+#Speech Recognition
+recog1 = spr.Recognizer()
+mc = spr.Microphone()
+
+class SpeechRecognition:
+    def recognize_speech(recog, source):
+        try:
+            recog.adjust_for_ambient_noise(source, duration=0.2)
+            audio = recog.listen(source)
+            recognized_text = recog.recognize_google(audio)
+            return recognized_text.lower()
+        except spr.UnknownValueError:
+            print("Google Speech Recognition could not understand the audio.")
+            return None
+        except spr.RequestError as e:
+            print(f"Could not request results from Google Speech Recognition service; {e}")
+            return None
+
+with mc as source:
+    print("Speak 'hello' to initiate the Translation!")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    MyText = recognize_speech(recog1, source)
+
+if MyText and 'hello' in MyText:
+    translator = Translator()
+    from_lang = 'en'
+    to_lang = 'hi'
+
+    with mc as source:
+        print("Speak a sentence to translate...")
+        get_sentence = recognize_speech(recog1, source)
+
+        if get_sentence:
+            try:
+                print(f"Phrase to be Translated: {get_sentence}")
+
+                text_to_translate = translator.translate(get_sentence, src=from_lang, dest=to_lang)
+                translated_text = text_to_translate.text
+
+                speak = gTTS(text=translated_text, lang=to_lang, slow=False)
+
+                speak.save("captured_voice.mp3")
+
+                os.system("start captured_voice.mp3")
+
+            except Exception as e:
+                print(f"An error occurred: {e}")
+        else:
+            print("Unable to capture the sentence for translation.")
 
 
 #Application Icon
